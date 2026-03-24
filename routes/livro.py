ce2fce02337acb73a9771e6b7aeb2c58fd8d3f10
ismecris <cristielythainara@gmail.com>
2026-03-23 22:04:52 -0300

    adicionando edição e salvamento de livros

--- routes/livro.py
+++ routes/livro.py
@@ -1,20 +1,55 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from database.models.livro import Livro
+from werkzeug.utils import secure_filename
+import os
 
 livro_route = Blueprint('livro', __name__)
+UPLOAD_FOLDER = 'static/uploads/capas'
 
 @livro_route.route('/')
 def lista_livros():
-    todos_os_livros= Livro.select()
+    todos_os_livros = Livro.select()
     return render_template('livros.html', livros=todos_os_livros)
 
 @livro_route.route('/salvar', methods=['POST'])
 def salvar_livro():
+    capa_url = request.form.get('capa_url')
+
+    arquivo = request.files.get('file_upload')
+    if arquivo and arquivo.filename != '':
+        filename = secure_filename(arquivo.filename)
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+        caminho = os.path.join(UPLOAD_FOLDER, filename)
+        arquivo.save(caminho)
+        capa_url = '/' + caminho
+
     Livro.create(
         titulo=request.form.get('titulo'),
         autor=request.form.get('autor'),
         sinopse=request.form.get('descricao'), 
-        capa_url=request.form.get('capa_url')
+        capa_url=capa_url
     )
+    return redirect(url_for('livro.lista_livros'))
 
+@livro_route.route('/salvar', methods=['POST'])
+def salvar_livro_json():
+    capa_url = request.form.get('capa_url')
+    
+    arquivo = request.files.get('file_upload')
+    if arquivo and arquivo.filename != '':
+        filename = secure_filename(arquivo.filename)
+      
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+        
+        caminho_no_disco = os.path.join(UPLOAD_FOLDER, filename)
+        arquivo.save(caminho_no_disco)
+        
+        capa_url = '/static/uploads/capas/' + filename
+
+    Livro.create(
+        titulo=request.form.get('titulo'),
+        autor=request.form.get('autor'),
+        sinopse=request.form.get('descricao'), 
+        capa_url=capa_url
+    )
     return redirect(url_for('livro.lista_livros'))
