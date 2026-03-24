8bdd7a584624e91e14f979de1b6142be1ee7bd6e
ismecris <cristielythainara@gmail.com>
2026-03-23 22:18:05 -0300

    Corrigindo rota dos livros

--- routes/livro.py
+++ routes/livro.py
@@ -19,27 +19,6 @@ def salvar_livro():
     if arquivo and arquivo.filename != '':
         filename = secure_filename(arquivo.filename)
         os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-        caminho = os.path.join(UPLOAD_FOLDER, filename)
-        arquivo.save(caminho)
-        capa_url = '/' + caminho
-
-    Livro.create(
-        titulo=request.form.get('titulo'),
-        autor=request.form.get('autor'),
-        sinopse=request.form.get('descricao'), 
-        capa_url=capa_url
-    )
-    return redirect(url_for('livro.lista_livros'))
-
-@livro_route.route('/salvar', methods=['POST'])
-def salvar_livro_json():
-    capa_url = request.form.get('capa_url')
-    
-    arquivo = request.files.get('file_upload')
-    if arquivo and arquivo.filename != '':
-        filename = secure_filename(arquivo.filename)
-      
-        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
         
         caminho_no_disco = os.path.join(UPLOAD_FOLDER, filename)
         arquivo.save(caminho_no_disco)
