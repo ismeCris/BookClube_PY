9e44ac938936ba9005a047b9183fd7c3a43bba7f
ismecris <cristielythainara@gmail.com>
2026-03-23 22:43:30 -0300

    adicionando  edição dos livros e capas dos mesmos

--- routes/livro.py
+++ routes/livro.py
@@ -16,19 +16,55 @@ def salvar_livro():
     capa_url = request.form.get('capa_url')
 
     arquivo = request.files.get('file_upload')
+
+    # 👉 PRIORIDADE: arquivo do PC
     if arquivo and arquivo.filename != '':
         filename = secure_filename(arquivo.filename)
+
         os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-        
-        caminho_no_disco = os.path.join(UPLOAD_FOLDER, filename)
-        arquivo.save(caminho_no_disco)
-        
+
+        caminho = os.path.join(UPLOAD_FOLDER, filename)
+        arquivo.save(caminho)
+
         capa_url = '/static/uploads/capas/' + filename
+
+    if not capa_url:
+        capa_url = '/static/images/minha_capa_padrao.jpg'
 
     Livro.create(
         titulo=request.form.get('titulo'),
         autor=request.form.get('autor'),
-        sinopse=request.form.get('descricao'), 
+        sinopse=request.form.get('descricao'),
         capa_url=capa_url
     )
+
+    return redirect(url_for('livro.lista_livros'))
+
+@livro_route.route('/<int:id>/editar', methods=['POST'])
+def editar_livro(id):
+    livro = Livro.get_by_id(id)
+
+    capa_url = request.form.get('capa_url')
+    arquivo = request.files.get('file_upload')
+
+    # Se enviou nova imagem
+    if arquivo and arquivo.filename != '':
+        filename = secure_filename(arquivo.filename)
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+        caminho = os.path.join(UPLOAD_FOLDER, filename)
+        arquivo.save(caminho)
+
+        capa_url = '/static/uploads/capas/' + filename
+
+    # Atualiza os dados
+    livro.titulo = request.form.get('titulo')
+    livro.autor = request.form.get('autor')
+    livro.sinopse = request.form.get('descricao')
+
+    if capa_url:
+        livro.capa_url = capa_url
+
+    livro.save()
+
     return redirect(url_for('livro.lista_livros'))
