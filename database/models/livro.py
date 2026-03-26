1f7921abe956a741914feed6892dffd83b522565
ismecris <cristielythainara@gmail.com>
2026-03-25 21:10:31 -0300

    Alterando tabela de livros

--- database/models/livro.py
+++ database/models/livro.py
@@ -1,17 +1,28 @@
-from peewee import Model, CharField, DateTimeField, IntegerField, TextField
+from peewee import Model, CharField, DateTimeField, IntegerField, TextField, ForeignKeyField, BooleanField
 from database.database import db
-import datetime
+from datetime import datetime
+from database.models.usuario import Usuario
 
 class Livro(Model):
+    STATUS_EXPLORAR = 'explorar'
+    STATUS_LIDO = 'lido'
+    STATUS_FAVORITO = 'favorito'
+
     titulo = CharField()
     autor = CharField()
     editora = CharField(null=True)
-    num_pag = IntegerField(null=True)
+    num_paginas = IntegerField(null=True)
     genero = CharField(null=True)
     sinopse = TextField(null=True)
     capa_url = CharField(null=True)
 
-    data_registro = DateTimeField(default=datetime.datetime.now)
+    status = CharField(default=STATUS_EXPLORAR)
+    usuario = ForeignKeyField(Usuario, backref='livros')
+
+    avaliacao = IntegerField(default=0)
+    favorito = BooleanField(default=False)
+
+    data_registro = DateTimeField(default=datetime.now)
 
     class Meta:
         database = db
